82c74f2c47f068b48208157ba28f403cd7873b85
Rinaldo Pasa <rinaldopasa97@gmail.com>
2022-11-21 13:53:05 +0800

    Fish config: add fish config

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Disable greeting
+set -U fish_greeting
---
+++ install/install-fish.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# +------+
+# | fish |
+# +------+
+ln -sf "$HOME/.dotfiles/fish/config.fish" "$HOME/.config/fish/config.fish"
+
+ln -sf "$HOME/.dotfiles/fish/functions/su.fish" "$HOME/.config/fish/functions/su.fish"

2a11ec041177bd006bce40f8cfe4c50224de5d02
Rinaldo Pasa <rinaldopasa97@gmail.com>
2022-11-22 16:47:25 +0800

    fish config: Set Autostart sway

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,12 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 # Disable greeting
 set -U fish_greeting
+
+# zoxide - A smarter cd command
+zoxide init fish | source
+
+if status is-interactive
+    # Autostart sway
+    if test -z "$DISPLAY" -a (tty) = "/dev/tty1"
+        exec sway
+    end
+end

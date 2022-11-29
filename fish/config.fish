1c71f07fe7e72203abde12c339b4386cb8b32a33
Rinaldo Pasa <rinaldopasa97@gmail.com>
2022-11-29 10:36:47 +0800

    Set environment variables CARGO_HOME & VOLTA_HOME

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,12 @@ set fish_color_command magenta
 # EDITOR
 set -gx EDITOR "vim"
 
+# Volta | N: Already set in .bash_profile
+#set -gx VOLTA_HOME "$HOME/.volta"
+#set -gx CARGO_HOME "$HOME/.cargo"
+#set -gx PATH "$VOLTA_HOME/bin" $PATH
+#set -gx PATH "$CARGO_HOME/bin" $PATH
+
 # zoxide ? A smarter cd command
 zoxide init fish | source
 

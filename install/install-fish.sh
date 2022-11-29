0fbaf86e120c6b014cc57ca8cd36ec9854184bbb
Rinaldo Pasa <rinaldopasa97@gmail.com>
2022-11-29 23:26:52 +0800

    Mostly added colorschemes

--- install/install-fish.sh
+++ install/install-fish.sh
@@ -5,6 +5,7 @@
 # +------+
 ln -sf "$HOME/.dotfiles/fish/config.fish" "$HOME/.config/fish/config.fish"
 ln -sf "$HOME/.dotfiles/fish/starship.fish" "$HOME/.config/fish/conf.d/starship.fish"
+ln -sf "$HOME/.dotfiles/fish/colorschemes/tokyonight_storm.fish" "$HOME/.config/fish/conf.d/tokyonight_storm.fish"
 
 ln -sf "$HOME/.dotfiles/fish/functions/su.fish" "$HOME/.config/fish/functions/su.fish"
 

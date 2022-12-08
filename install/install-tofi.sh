54e6c7df2f435f45d67f5f1d75a4d2d39a927cd6
Rinaldo Pasa <rinaldopasa97@gmail.com>
2022-12-08 11:23:02 +0800

    Switch to i3: Remove all sway config

--- install/install-tofi.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# +------+
-# | Tofi |
-# +------+
-mkdir -p "$HOME/.config/tofi"
-ln -sf "$HOME/.dotfiles/tofi/config-drun" "$HOME/.config/tofi/config-drun"
-ln -sf "$HOME/.dotfiles/tofi/config-clipman" "$HOME/.config/tofi/config-clipman"
--- install/install-waybar.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# +--------+
-# | Waybar |
-# +--------+
-ln -s "$HOME/.dotfiles/waybar" "$HOME/.config/waybar"
